23fd33c9a2f3aedb6184826eab390728b3960c86
Andriy-Shymkiv <winamondezik@gmail.com>
2024-04-02 16:20:19 +0300

    add time depend on database

--- src/modules/health/controller.ts
+++ src/modules/health/controller.ts
@@ -1,11 +1,18 @@
 import constant from '../../constants/config';
+import sequelize from '../../database';
 import Sync from '../../database/models/sync.model';
 import { Request, Response } from 'express';
 
 async function checkHealth() {
+    // pg 2024-04-02T09:49:50.000Z
+    // mysql 2024-04-02T12:50:17.000Z
     const maxBlockTime = await Sync.max<number, Sync>('block_time');
 
-    const block_time = Date.parse(maxBlockTime.toString());
+    const block_time = new Date(
+        sequelize.dialect.name === 'postgres'
+            ? maxBlockTime + 'Z'
+            : maxBlockTime
+    ).getTime();
     const now = new Date().getTime();
     const timeDiff = now - block_time;
     const status =
@@ -18,9 +25,9 @@ async function checkHealth() {
 }
 
 export const health = async (_req: Request, res: Response) => {
-    const retVal = await checkHealth();
-    res.status(retVal.status ? constant.HTTP_200_CODE : constant.HTTP_503_CODE);
-    res.send(retVal);
+    const health = await checkHealth();
+    res.status(health.status ? constant.HTTP_200_CODE : constant.HTTP_503_CODE);
+    res.send(health);
 };
 
 export const isHealthy = async (_req: Request, res: Response) => {
