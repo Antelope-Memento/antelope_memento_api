075aff14f51930943cd5396b80a4d654a05702a1
Andriy-Shymkiv <winamondezik@gmail.com>
2024-04-05 13:25:40 +0300

    add ts to history controller

--- src/modules/history/route.ts
+++ src/modules/history/route.ts
@@ -1,9 +1,9 @@
-const express = require('express');
-const constroller = require('./controller');
+import express from 'express';
+import * as controller from './controller';
 
 const router = express.Router();
 
-router.get('/get_account_history', constroller.get_account_history);
-router.get('/get_pos', constroller.get_pos);
+router.get('/get_account_history', controller.get_account_history);
+router.get('/get_pos', controller.get_pos);
 
-module.exports = router;
+export default router;
