b422e7593efa0e11b68d580c9b29c82718911d7f
MaksHladun <makshladun@gmail.com>
2024-04-25 19:00:39 +0300

    Add getNextBlockWithTransaction function to receipts service

--- src/services/receipts/index.ts
+++ src/services/receipts/index.ts
@@ -24,3 +24,30 @@ export async function getCount({
         col: 'seq',
     });
 }
+
+export async function getNextBlockWithTransaction({
+    accounts,
+    fromBlock,
+}: {
+    accounts: string[];
+    fromBlock: number;
+}) {
+    const firstBlock = await Receipt.findOne({
+        where: {
+            block_num: {
+                [Op.gt]: fromBlock,
+            },
+            receiver: {
+                [Op.in]: accounts,
+            },
+        },
+        order: [['block_num', 'ASC']],
+        limit: 1,
+    });
+
+    if (firstBlock) {
+        return Number(firstBlock?.dataValues?.block_num);
+    } else {
+        return fromBlock;
+    }
+}
